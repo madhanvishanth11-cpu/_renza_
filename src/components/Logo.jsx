55bed1fac00cbe398afaca379b6e115db0ebd7b4
madhanvishanth11-cpu <madhanvishanth11@gmail.com>
2026-08-24 20:26:37 +0530

    Refine RENZA R logo path to match user reference silhouette exactly

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -9,13 +9,13 @@ export default function Logo({ className = "", size = "normal" }) {
 
   return (
     <div className={`flex items-center gap-2 select-none ${className}`}>
-      {/* Yellow geometric icon: Stylized modern R */}
+      {/* Yellow geometric icon: Stylized solid R silhouette with flat bottom */}
       <svg 
         viewBox="0 0 24 24" 
         className={`${isSmall ? 'h-[18px] w-[18px]' : 'h-[24px] w-[24px]'} fill-[#FFE500] flex-shrink-0`}
         xmlns="http://www.w3.org/2000/svg"
       >
-        <path d="M6 3.5A1.5 1.5 0 0 1 7.5 2h5.5a5 5 0 0 1 5 5c0 2.5-1.8 4.2-4 4.8l3.8 7.4a1 1 0 0 1-.9 1.8h-3.5a1 1 0 0 1-.9-.5L10 13h-1v6.5A1.5 1.5 0 0 1 7.5 21H7a1 1 0 0 1-1-1V3.5z" />
+        <path d="M6 20V5.5A3.5 3.5 0 0 1 9.5 2h4.5A5 5 0 0 1 19 7v3.5c0 2.5-2 3.5-4 3.5 2 0 3 2 3 6v1H6z" />
       </svg>
       {/* Logo Text */}
       <span className={`font-sans font-black tracking-tight leading-none ${isSmall ? 'text-base' : 'text-xl'} ${textColorClass}`}>
